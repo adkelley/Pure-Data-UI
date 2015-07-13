d011af61ed0caca80da18c75552f7e57fcdc80b2
Alex Kelley <adkelley@mac.com>
2015-07-13 09:09:44 -0700

    webpd added

--- app/index.js
+++ app/index.js
@@ -7,12 +7,13 @@ var app = express();
 app.use(bodyParser.urlencoded({extend: true}));
 app.use(express.static("app/public"));
 app.use(express.static("app/bower_components"));
+app.use(express.static("app/vendor"));
 
 var views = path.join(process.cwd(), "app/public/views");
 
 app.get('/', function(req, res) {
-  var homePath = path.join(views, "index.html");
-  res.sendFile(homePath);
+  var indexPath = path.join(views, "index.html");
+  res.sendFile(indexPath);
 });
 
 var server = app.listen(3000, function() {
